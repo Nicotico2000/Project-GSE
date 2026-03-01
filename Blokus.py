7506ce0d840935f50c9942a93758e18d85225613
Nicolas Alejandro Zevallos Chavez <nicolas.zevallos.chavez.2000@gmail.com>
2026-03-01 14:55:15 +0100

    Blokus Game

--- Blokus.py
+++ Blokus.py
@@ -1,12 +1,13 @@
 import tkinter as tk
+from functools import partial
 
+# We can use this to later handle the button to put the pieces
+# For now, it just prints the coordinates of the button clicked
 def on_click(row, col):
     print(f"Button clicked at ({row}, {col})")
 
 root = tk.Tk()
-root.title("10x10 Board")
-
-# Create a 10x10 grid of buttons
+root.title("Blokus 20x20 board")
 buttons = []
 
 for i in range(20):
@@ -14,8 +15,8 @@ for i in range(20):
     for j in range(20):
         btn = tk.Button(
             root,
-            text=f"{i},{j}",
-            width=2,
+            text=f"{i,j}", #para tenerlo numerado
+            width=4,
             height=1,
             command=lambda r=i, c=j: on_click(r, c)
         )
